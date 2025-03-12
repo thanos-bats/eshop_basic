a704da126aa0cf319835d521f9b14100e4bed319
thanos-bats <thanos.batsiou@gmail.com>
2025-03-12 18:16:01 +0200

    Create Flask app and configure database connection in app

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+from database.models import db
+from routes.user_routes import user_bp
+from routes.products_routes import products_bp
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+db.init_app(app)
+jwt = JWTManager(app)
+
+app.register_blueprint(user_bp)
+app.register_blueprint(products_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=4567)

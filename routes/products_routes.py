888b4e1d3b925d98acf3d976db78bc6fc27865db
thanos-bats <thanos.batsiou@gmail.com>
2025-03-13 17:36:21 +0200

    Public dashboard router

--- routes/products_routes.py
+++ routes/products_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from services.products_services import get_all_products
+from services.products_services import get_all_products, get_public_dashboard
 
 products_bp = Blueprint('product_bp', __name__)
 
@@ -8,3 +8,9 @@ def get_products_route():
     products = get_all_products()
 
     return jsonify(products), 200
+
+@products_bp.route('/public/dashboard', methods=['GET'])
+def get_public_dashboard_route():
+    stats = get_public_dashboard()
+
+    return jsonify(stats), 200

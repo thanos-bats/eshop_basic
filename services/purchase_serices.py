5ca725675cc44b73f64f29fe4ef7d56ff17245b8
thanos-bats <thanos.batsiou@gmail.com>
2025-03-13 17:18:45 +0200

    Round the total amount of a purchase

--- services/purchase_serices.py
+++ services/purchase_serices.py
@@ -35,11 +35,10 @@ def create_purchase(jwt_identity, products_data):
             )
         )
 
-    purchase.total_amount = purchase_total_amount
+    purchase.total_amount = round(purchase_total_amount, 2)
     db.session.commit()
 
     return {"message": "Purchase created successfully.", "id": purchase.id}, 201
-
 
 def get_purchases(jwt_identity):
     purchases = Purchase.query.filter_by(user_id=jwt_identity).all()

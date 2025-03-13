e7713d7b982cebf7d42bfca347c72a2b88c299f5
thanos-bats <thanos.batsiou@gmail.com>
2025-03-13 11:04:43 +0200

    Handle error in user routes

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -5,12 +5,19 @@ user_bp = Blueprint('user_bp', __name__)
 
 @user_bp.route('/register', methods=['POST'])
 def register_route():
-    data = request.json
-    response = register_user(data)
-    return jsonify(response)
+    try:
+        data = request.json
+        response, status_code = register_user(data)
+        return jsonify(response), status_code
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
 @user_bp.route('/login', methods=['POST'])
 def login_route():
-    data = request.json
-    response = login_user(data)
-    return jsonify(response)
+    try:
+        data = request.json
+        response, status_code = login_user(data)
+        return jsonify(response), status_code
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+    
